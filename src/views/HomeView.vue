b85e01b9439d76f65c602b7b7dce2923c5cc2bd2
Gareth <bravdthepally@gmail.com>
2023-06-07 21:32:44 +0200

    Added tailwind

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <main>
+        <div class="text-3xl font-bold underline">Welcome to Miniature Eureka</div>
+    </main>
 </template>
