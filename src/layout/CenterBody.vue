72eba07faacd9cefdb1a6461903b020f9d792712
Gareth Palmer <bravdthepally@gmail.com>
2023-06-17 21:54:07 +0200

    Updated routerview

--- src/layout/CenterBody.vue
+++ src/layout/CenterBody.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
     <section class="grow-[4] p-8 bg-blue-200 mt-4 rounded-md shadow-inner">
-        <router-view></router-view>
+        <RouterView />
     </section>
 </template>
