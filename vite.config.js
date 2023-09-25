1094e05c51ea82da1892737f07735ceae8984c24
Gareth Palmer <bravdthepally@gmail.com>
2023-09-25 08:58:40 +0200

    Fixed import bug

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import { fileURLToPath } from 'url';
 
 export default defineConfig({
     plugins: [
