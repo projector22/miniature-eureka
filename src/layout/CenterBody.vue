b2b2b8d1ba3e3667f185e281e4022d3487765ccb
Gareth Palmer <bravdthepally@gmail.com>
2023-11-25 12:53:15 +0200

    Removed old build

--- src/layout/CenterBody.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router';
-</script>
-
-<template>
-    <section class="grow-[4] p-8 bg-blue-200 mt-4 rounded-md shadow-inner">
-        <RouterView />
-    </section>
-</template>
--- src/views/HomeView.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h2 class="text-2xl font-semibold mb-2">Home</h2>
-</template>
